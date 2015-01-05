5d04f05e91c74fa391f51c8c4e64c3f19c9fe2be
Joe Collins <joejcollins@gmail.com>
2015-01-05 19:41:03 +0000

    Third menu of the new era.

--- ltx2xml.py
+++ ltx2xml.py
@@ -2,11 +2,11 @@ import re
 processed_string = open('Original LaTeX Food File.tex').read()
 
 # Recipes
-match_string = re.compile(ur'\\recipe{([a-zA-Z0-9_ ]*)}%')
+match_string = re.compile(ur'\\recipe{([a-zA-Z0-9_ ]*)}')
 substitution_string = ur'<Recipe Title="\1" Meals="2">'
 processed_string = re.sub(match_string, substitution_string, processed_string)
 
-match_string = re.compile(ur'\\quick{([a-zA-Z0-9_ ]*)}%')
+match_string = re.compile(ur'\\quick{([a-zA-Z0-9_ ]*)}')
 substitution_string = ur'<Recipe Title="\1" Meals="1">'
 processed_string = re.sub(match_string, substitution_string, processed_string)
 
@@ -22,12 +22,17 @@ for key, value in ingredients.iteritems():
     substitution_string = ur'<' + value + ' Quantity="\\1" Unit="\\2" Type="\\3" Process="\\4"/>'
     processed_string = re.sub(match_string, substitution_string, processed_string)
 
+#Remove Process="" and Unit=""
+
+
 # Utensils
 utensils = {'cass': 'casserole', 'bowl': 'bowl', 'pan': 'pan', 'wok': 'wok'};
 for key, value in utensils.iteritems():
     match_string = re.compile(ur'\\' + key)
     substitution_string = u'<Utensil Name=\"' + value + '\" />'
     processed_string = re.sub(match_string, substitution_string, processed_string )
+
+# replace \pasta{spaghetti} with <Staple Name="spaghetti"/>
 
 outfile = open('Original LaTeX Food File.xml', 'w')
 
