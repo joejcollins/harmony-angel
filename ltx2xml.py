1296c71324727042beeda9ff1f16046a7ac5c98b
Joe Collins <joejcollins@gmail.com>
2015-01-05 12:02:05 +0000

    Python script to convert from LaTeX to XML.

---
+++ ltx2xml.py
@@ -0,0 +1,35 @@
+import re
+processed_string = open('Original LaTeX Food File.tex').read()
+
+# Recipes
+match_string = re.compile(ur'\\recipe{([a-zA-Z0-9_ ]*)}%')
+substitution_string = ur'<Recipe Title="\1" Meals="2">'
+processed_string = re.sub(match_string, substitution_string, processed_string)
+
+match_string = re.compile(ur'\\quick{([a-zA-Z0-9_ ]*)}%')
+substitution_string = ur'<Recipe Title="\1" Meals="1">'
+processed_string = re.sub(match_string, substitution_string, processed_string)
+
+# Stage
+match_string = re.compile(ur'\\st')
+substitution_string = u"<Stage>"
+processed_string = re.sub(match_string, substitution_string, processed_string)
+
+# Ingredients
+ingredients = {'ve': 'Vegetable', 'ch': 'Check', 'da': 'Dairy', 'gr': 'Grocery', 'me': 'Meat', 'wa': 'Water'};
+for key, value in ingredients.iteritems():
+    match_string = re.compile(ur'\\' + key + '{([a-zA-Z0-9_. ]*)}{([a-zA-Z0-9_ ]*)}{([a-zA-Z0-9_ ]*)}{([a-zA-Z0-9_ ]*)}')
+    substitution_string = ur'<' + value + ' Quantity="\\1" Unit="\\2" Type="\\3" Process="\\4"/>'
+    processed_string = re.sub(match_string, substitution_string, processed_string)
+
+# Utensils
+utensils = {'cass': 'casserole', 'bowl': 'bowl', 'pan': 'pan', 'wok': 'wok'};
+for key, value in utensils.iteritems():
+    match_string = re.compile(ur'\\' + key)
+    substitution_string = u'<Utensil Name=\"' + value + '\" />'
+    processed_string = re.sub(match_string, substitution_string, processed_string )
+
+outfile = open('Original LaTeX Food File.xml', 'w')
+
+outfile.write(processed_string)
+outfile.close()
